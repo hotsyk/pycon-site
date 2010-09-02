21c1736ade364d337eadcd11fc13216d6bfa28b1
volodymyr Hotsyk <gotsyk@gmail.com>
2010-09-02 19:50:40 +0300

    Admin update

--- pycon/core/admin.py
+++ pycon/core/admin.py
@@ -1,9 +1,19 @@
 from django.contrib import admin
 from pycon.core.models import *
 
+def barcode(obj):
+    return obj.ticket_barcode != ''
+barcode.short_description = 'Barcode'
 
+class ParticipanProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'tshirt_size', 'pre_party',
+                    'country', 'city', 'python_level',
+                    'python_years', 'active',
+                     barcode)
+        
+        
 admin.site.register(Page)
 admin.site.register(News)
 admin.site.register(Speaker)
 admin.site.register(CFPProfile)
-admin.site.register(ParticipanProfile)
+admin.site.register(ParticipanProfile, ParticipanProfileAdmin)
