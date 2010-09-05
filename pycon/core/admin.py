1db1ec3f413ab1414ae2ef071222d6a610c9f640
volodymyr Hotsyk <gotsyk@gmail.com>
2010-09-06 00:21:08 +0300

    Completed property in admin

--- pycon/core/admin.py
+++ pycon/core/admin.py
@@ -5,11 +5,15 @@ def barcode(obj):
     return obj.ticket_barcode != ''
 barcode.short_description = 'Barcode'
 
+def is_profile_complete(obj):
+    return obj.is_profile_completed
+is_profile_complete.short_description = 'Completed'
+
 class ParticipanProfileAdmin(admin.ModelAdmin):
     list_display = ('user', 'tshirt_size', 'pre_party',
                     'country', 'city', 'python_level',
                     'python_years', 'active',
-                     barcode)
+                     barcode, is_profile_complete)
         
         
 admin.site.register(Page)
